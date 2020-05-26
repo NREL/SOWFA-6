06c54a3816ac5487a14b19f19bda5495a3446aee
Regis Thedin <registhedin@gmail.com>
2020-05-26 14:35:58 -0600

    Add call to update sponge layers into the solver

--- applications/solvers/superDeliciousVanilla/superDeliciousVanilla.C
+++ applications/solvers/superDeliciousVanilla/superDeliciousVanilla.C
@@ -99,6 +99,9 @@ int main(int argc, char *argv[])
             momentumSourceTerm.update(pimple.finalPimpleIter());
             temperatureSourceTerm.update(pimple.finalPimpleIter());
 
+            // Update damping layers forcing
+            spongeLayers.update();
+
             // Predictor step.
             Info << "   Predictor" << endl;
 
